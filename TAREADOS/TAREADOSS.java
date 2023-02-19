c23d90cd2cf7073333e69491fe737e635c63ed08
CamChavez <camilachavez320@aragon.unam.mx>
2023-02-19 17:11:47 -0600

    tareados

---
+++ TAREADOS/TAREADOSS.java
@@ -0,0 +1,10 @@
+
+public class tareados{
+
+   public static void main(String [] args){
+        String cadena = "Camila Estudihambre México";
+        System.out.println("MI NOMBRE ES: " + cadena.substring(0,6));
+        System.out.println("PROFESIÓN: " + cadena.substring(7,19));
+        System.out.println("SOY DE: " + cadena.substring(20,26));
+   }
+}
